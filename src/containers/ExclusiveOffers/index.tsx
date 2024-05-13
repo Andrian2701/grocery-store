755309c55a673947e19e72621adad6a9d4b88003
Andrian Smetaniuk <andrian.smet@gmail.com>
2024-05-13 16:25:10 +0300

    add LoadingSkeleton

--- src/containers/ExclusiveOffers/index.tsx
+++ src/containers/ExclusiveOffers/index.tsx
@@ -1,5 +1,5 @@
 import { Link, useParams } from "react-router-dom";
-import { Box, Button, Typography, useTheme } from "@mui/material";
+import { Box, Button, Typography, Skeleton, useTheme } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
@@ -85,7 +85,47 @@ export const ExclusiveOffers = () => {
               </SwiperSlide>
             ))
           ) : (
-            <h1>Loading</h1>
+            <>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <SwiperSlide key={index}>
+                  <Skeleton
+                    variant="rectangular"
+                    animation="pulse"
+                    sx={{
+                      width: "100%",
+                      height: { xs: 130, sm: 150 },
+                    }}
+                  />
+                  <Box width="100%">
+                    <Skeleton
+                      variant="text"
+                      animation="pulse"
+                      width="100%"
+                      height={23}
+                    />
+                    <Skeleton
+                      variant="text"
+                      animation="pulse"
+                      width="100%"
+                      height={23}
+                    />
+                  </Box>
+                  <Box
+                    width="100%"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Skeleton
+                      variant="text"
+                      animation="pulse"
+                      width={52}
+                      height={23}
+                    />
+                  </Box>
+                </SwiperSlide>
+              ))}
+            </>
           )}
         </Swiper>
       </Box>
